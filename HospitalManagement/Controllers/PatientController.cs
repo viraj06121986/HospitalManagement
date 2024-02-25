746102a9a3b2a52b943d3d6dfbfb4a3a794dfbb6
virajboralkar <79639197+virajboralkar@users.noreply.github.com>
2024-02-25 08:19:43 +0530

    Add project files.

---
+++ HospitalManagement/Controllers/PatientController.cs
@@ -0,0 +1,35 @@
+﻿using HospitalManagement.Models.UserRole;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HospitalManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientController : Controller
+    {
+        [HttpGet("Id")]
+        public JsonResult Index(int Id)
+        {
+            var options = new JsonSerializerOptions();
+            options.PropertyNamingPolicy = null;
+            List<Patient> patients = new List<Patient> { 
+                new Patient { Id = 101, FirstName = "Viraj", DateOfBirth = DateOnly.ParseExact("06/12/1986","dd/mm/yyyy")}
+            };
+            return Json(patients,options);
+        }
+
+        [HttpGet("Name")]
+        public JsonResult PatientWithName(string Name)
+        {
+            var options = new JsonSerializerOptions();
+            options.PropertyNamingPolicy = null;
+            List<Patient> patients = new List<Patient> {
+                new Patient { Id = 101, FirstName = "Viraj", DateOfBirth = DateOnly.ParseExact("06/12/1986","dd/mm/yyyy")}
+            };
+            return Json(patients, options);
+        }
+    }
+}
---
+++ HospitalManagement/Models/UserRole/Patient.cs
@@ -0,0 +1,28 @@
+﻿namespace HospitalManagement.Models.UserRole
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public int  Age { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? EmailId { get; set; }
+        public string? AddressLine1 { get; set; }
+        public string? AddressLine2 { get; set; }
+        public string? AddressLine3 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? UserType { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? CreatedOn { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public string? LastModifiedOn { get; set; }
+
+
+
+    }
+}
